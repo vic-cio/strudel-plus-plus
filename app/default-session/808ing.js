8141951271f1a0e1a27da83a64510c01dca30825
Victor <131465041+vic-cio@users.noreply.github.com>
2026-09-05 16:11:20 +0100

    Refresh bundled we cook beat snapshot from live session (#23)

--- app/default-session/808ing.js
+++ app/default-session/808ing.js
@@ -1,7 +1,7 @@
 stack(
   s("bd ~ ~ bd ~ bd ~ ~").gain(0.3).distort(0.6).shape(0.8).sustain(0.9).compressor(0.9),
-  s("~ ~ cp ~ ~ ~ cp ~").gain(0.7),
-  s("hh*8?").gain(0.85).sometimesBy(0.1, x=>x.fast(2)).pan(sine.slow(4)),
+  s("~ ~ cp ~ ~ ~ cp ~").gain(0.6),
+  s("hh*8?").gain(1).sometimesBy(0.1, x=>x.fast(2)).pan(sine.slow(4)),
   note("c1 ~ ~ c1 eb1 ~ c1 ~").s("sawtooth")
     .lpf(300).lpq(8).distort(0.5).shape(0.3).gain(0.6)
     .slide(0.3).release(0.13),
